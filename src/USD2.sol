1afc2f4a95c1003da89126d7df88d70eb60f2eca
nourharidy <nourharidy@yahoo.com>
2024-08-01 10:03:53 +0300

    fix accrueInterest

--- src/USD2.sol
+++ src/USD2.sol
@@ -219,7 +219,7 @@ contract USD2 is ERC20 {
         );
     
         // update debt
-        uint interest = totalPaidDebt * rateIntegral / 1e18 / 365 days;
+        uint interest = totalPaidDebt * rateIntegral / 1e18;
 
         // update debt
         totalPaidDebt += interest;
