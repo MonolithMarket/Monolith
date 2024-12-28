5ad93d7d92b909be51cebd8dc5d78e2a9c59d790
nourharidy <nourharidy@yahoo.com>
2024-12-28 05:07:56 +0200

    add factory tests

--- test/Factory.t.sol
+++ test/Factory.t.sol
@@ -5,6 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {Factory} from "src/Factory.sol";
 import  "lib/solmate/src/tokens/ERC20.sol";
 import {USD2} from "src/USD2.sol";
+import {SUSD2} from "src/SUSD2.sol";
 
 contract MockCollateral is ERC20 {
     constructor() ERC20("MockCollateral", "MCOLL", 18) {}
@@ -18,12 +19,19 @@ contract FactoryTest is Test {
         address feed = address(1);
         address operator = address(2);
         Factory factory = new Factory(collateral, feed, operator);
+
         assertNotEq(factory.usd2(), address(0));
         assertNotEq(factory.susd2(), address(0));
+
+        assertEq(address(USD2(factory.usd2()).sUSD2()), factory.susd2());
+        assertEq(address(SUSD2(factory.susd2()).asset()), factory.usd2());
+
         assertEq(address(USD2(factory.usd2()).collateral()), collateral);
         assertEq(address(USD2(factory.usd2()).feed()), feed);
+        assertEq(USD2(factory.usd2()).symbol(), "USD2");
         assertEq(USD2(factory.usd2()).operator(), operator);
+
         assertEq(USD2(factory.susd2()).operator(), operator);
-        
+        assertEq(USD2(factory.susd2()).symbol(), "sUSD2");
     }
 }
