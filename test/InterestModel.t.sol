aa6d2faf31a46aa95eee9c3c69ae69dfc7a2b5f6
Nour Haridy <nourharidy@yahoo.com>
2026-02-25 04:43:41 -0800

    fix: prevent division-by-zero when wadExp underflows to 0 (#32)

--- test/InterestModel.t.sol
+++ test/InterestModel.t.sol
@@ -48,4 +48,30 @@ contract InterestModelTest is Test {
         assertApproxEqRel(currBorrowRate, expectedBorrowRate, 1e11, "borrow rate mismatch");
         assertApproxEqRel(interest, expectedInterest, 1e15, "interest mismatch");
     }
+
+    function test_calculateInterest_wadExpUnderflow() public view {
+        uint totalPaidDebt = 1e18;
+        uint lastRate = 5e16;
+        uint timeElapsed = 60 days;
+        uint halfLife = 12 hours;
+        uint expRate = uint(wadLn(2e18)) / halfLife;
+        uint lastFreeDebtRatioBps = 1000;
+        uint targetFreeDebtRatioStartBps = 2500;
+        uint targetFreeDebtRatioEndBps = 7500;
+
+        (uint currBorrowRate, uint interest) = interestModel.calculateInterest(
+            totalPaidDebt,
+            lastRate,
+            timeElapsed,
+            expRate,
+            lastFreeDebtRatioBps,
+            targetFreeDebtRatioStartBps,
+            targetFreeDebtRatioEndBps
+        );
+
+        // When growthDecay underflows to 0, we set it to 1, causing currBorrowRate
+        // to exceed uint88.max, triggering overflow protection: return (_lastRate, 0)
+        assertEq(currBorrowRate, lastRate);
+        assertEq(interest, 0);
+    }
 }
